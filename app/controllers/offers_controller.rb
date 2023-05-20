2b738d39cc246de820845b16e1b661e4c7f3c6a5
Murilo Gomes <mgfreires@hotmail.com>
2023-05-20 14:27:28 +0200

    changing db

---
+++ app/controllers/offers_controller.rb
@@ -0,0 +1,47 @@
+class OffersController < ApplicationController
+  def index
+    @offers = Offer.all
+    # @markers = @posts.geocoded.map do |post|
+    #   {
+    #     lat: post.latitude,
+    #     lng: post.longitude,
+    #     info_window_html: render_to_string(partial: "info_window", locals: { post: post }),
+    #     marker_html: render_to_string(partial: "marker", locals: { post: post })
+    #   }
+    end
+  end
+
+  def show
+    @offer = Offer.find(params[:id])
+    # @markers = [
+    #   {
+    #     lat: @post.latitude,
+    #     lng: @post.longitude,
+    #     info_window_html: render_to_string(partial: "info_window", locals: { post: @post }),
+    #     marker_html: render_to_string(partial: "marker", locals: { post: @post })
+    #   }]
+  end
+
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+    @user = current_user
+    @offer.user = @user
+
+    if @offer.save
+
+      redirect_to root_path
+
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:title, :body, :address, :post_image)
+  end
