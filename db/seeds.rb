b86986729e0b3c5b5d7806f7eb55e9a007cccafb
Laia Grobe De Luca <laia.gdla@gmail.com>
2023-05-20 14:43:36 +0200

    adjusted seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# require 'inquirer'
+
+# list_seeds = Dir[File.join(Rails.root, "db", "seeds", "*.rb")]
+
+# idx = Ask.list "Select a seed file to run:", list_seeds
+# puts "You selected: #{list_seeds[idx]}"
+
+# load list_seeds[idx]
+
+Dir.glob(File.join("*", "seeds", "*.rb")).each do |seed|
+  puts "\n🌱 Do you wish to run #{seed}: [Y,n]"
+  puts "➡️"
+  if gets.chomp.downcase == "n"
+    puts "🛸 Skipping file\n"
+  else
+  	load seed
+  end
+end
+
+puts "\n🌈 Finished!"
