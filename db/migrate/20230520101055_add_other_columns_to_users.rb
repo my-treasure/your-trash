824f972ba4a45c78093397dfc268fa6f15d9c689
Teo Maisuradze <tikomaisuradze92@gmail.com>
2023-05-20 14:44:51 +0200

    other changes

--- db/migrate/20230520101055_add_other_columns_to_users.rb
+++ db/migrate/20230520101055_add_other_columns_to_users.rb
@@ -2,7 +2,7 @@ class AddOtherColumnsToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :address, :string
     add_column :users, :longitude, :float
-    add_column :users, :lattitute, :float
+    add_column :users, :latitute, :float
     add_column :users, :role, :string
   end
 end
