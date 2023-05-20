19005dd33e549cc8368e2ce705c82ab8c268497b
Teo Maisuradze <tikomaisuradze92@gmail.com>
2023-05-20 13:06:29 +0200

    relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :offers
+  has_many :reviews
+  has_many :followers
+  has_many :messages
+  has_many :bookings
 end
