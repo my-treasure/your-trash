94ab6604e9b60a190a1e2cc0e5ca1a0f3793f766
Murilo Gomes <mgfreires@hotmail.com>
2023-05-27 17:09:42 +0200

    changes db

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "offers#index"
-  resources :offers
+  resources :bookings, except: [:new, :create]
+
+  resources :offers do
+    resources :bookings, only: [:new, :create]
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
