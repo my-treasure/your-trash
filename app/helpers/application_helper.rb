a9a4dfddfbe2b8bc55b4706585e33d5b12e87672
Laia Grobe De Luca <laia.gdla@gmail.com>
2023-06-07 20:44:47 +0200

    styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def address_text(entry)
+    entry.address&.split(",")[-4..-2]&.join
+  end
+
+  def distance_km(start, finish)
+    Geocoder::Calculations.distance_between([start.latitude, start.longitude], [finish.latitude, finish.longitude]).round(2)
+  end
 end
