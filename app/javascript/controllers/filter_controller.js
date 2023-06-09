a248ddce9d96017694afb9a82323d3d2b2db700d
Laia Grobe De Luca <laia.gdla@gmail.com>
2023-06-09 20:49:36 +0200

    searchbar and profile

--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -8,13 +8,27 @@ export default class extends Controller {
   connect() {
   }
 
-  toggleFilter(){
-    if (this.filtercontainerTarget.classList.contains("hiddennav")){
-      this.filtercontainerTarget.classList.remove("hiddennav")
-    } else {
-      this.filtercontainerTarget.classList.add("hiddennav")
-    }
+  // toggleFilter(){
+  //   if (this.filtercontainerTarget.classList.contains("hiddennavsearch")){
+  //     this.filtercontainerTarget.classList.remove("hiddennavsearch")
+  //   } else {
+  //     this.filtercontainerTarget.classList.add("hiddennavsearch")
+  //   }
+  // }
+
+  toggleFilter() {
+    this.filtercontainerTarget.classList.toggle("hiddennavsearch");
+    // if (this.filtercontainerTarget.classList.contains("hiddennavsearch")) {
+    //   this.filtercontainerTarget.addEventListener("transitionend", this.hideContainer);
+    // } else {
+    //   this.containerTarget.removeEventListener("transitionend", this.hideContainer);
+    //   this.filtercontainerTarget.style.display = "block";
+    // }
   }
+
+  // hideContainer = () => {
+  //   this.filtercontainerTarget.style.display = "none";
+  // };
 
   toggleProfileicon(){
     this.profileiconcontainerTarget.classList.toggle("hiddennav");
