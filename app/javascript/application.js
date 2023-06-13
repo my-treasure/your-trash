0279b8c0444312f14e0632476f8b3142ff813abb
Laia Grobe De Luca <laia.gdla@gmail.com>
2023-06-13 20:30:25 +0200

    js app

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,11 +4,11 @@ import "./controllers"
 import "bootstrap"
 
 
-// var slider = document.getElementById("myRange");
-// var output = document.getElementById("demo");
-// output.innerHTML = slider.value; // Display the default slider value
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
 
-// // Update the current slider value (each time you drag the slider handle)
-// slider.oninput = function () {
-//   output.innerHTML = this.value;
-// };
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function () {
+  output.innerHTML = this.value;
+};
