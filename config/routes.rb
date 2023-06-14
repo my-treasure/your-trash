d1156170f1204f012d0405197d3170df9f30adab
Laia Grobe De Luca <laia.gdla@gmail.com>
2023-06-14 10:11:02 +0200

    routes, messages, reviews

--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,6 @@ Rails.application.routes.draw do
   post 'profiles/:id', to: "pages#createprofile"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
